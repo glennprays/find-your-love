0237304fb0aeb93fe1b9abb18eaeced8bc4740a5
michael setiawan <93133742+michaelsetiawann@users.noreply.github.com>
2023-08-25 18:42:59 +0700

    feat: add wall collision

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ from pgzero.actor import Actor
 from pgzero.loaders import sounds
 import pygame
 from pgzero import clock
+import math
 
 TILE_SIZE = 32 
 WIDTH = 20 * TILE_SIZE 
@@ -49,8 +50,8 @@ def draw():
 
     screen.fill((70, 30, 50))
     create_map()
-    mimi.draw()
     tata.draw()
+    mimi.draw()
 
 def create_map():
     for row in range(len(maze)):
@@ -79,12 +80,23 @@ def move_right():
         mimi.x = target
 
 def update():
-    
-    if keyboard.up:
-        clock.schedule_unique(move_up,0.05)
-    elif keyboard.down:
-        clock.schedule_unique(move_down,0.05)
-    elif keyboard.left:
-        clock.schedule_unique(move_left,0.05)
-    elif keyboard.right:
-        clock.schedule_unique(move_right,0.05)
+    coloumn = math.floor((mimi.x / TILE_SIZE))
+    row = math.floor((mimi.y / TILE_SIZE))
+    # TODO: FINISH WIN GAME
+    if coloumn == 19 and row == 1:
+        exit() 
+        screen.clear()
+        screen.draw.text("You Win!", ((WIDTH/2), (HEIGHT/2)), color=(255,255,255), fontsize=70)
+
+    elif keyboard.up and row-1 >= 0:
+        if maze[row-1][coloumn] == 0:
+            clock.schedule_unique(move_up,0.05)
+    elif keyboard.down and row+1 < (HEIGHT/TILE_SIZE):
+        if maze[row+1][coloumn] == 0:
+            clock.schedule_unique(move_down,0.05)
+    elif keyboard.left and coloumn-1 > 0:
+        if maze[row][coloumn-1] == 0:
+            clock.schedule_unique(move_left,0.05)
+    elif keyboard.right and coloumn+1 < (WIDTH/TILE_SIZE):
+        if maze[row][coloumn+1] == 0:
+            clock.schedule_unique(move_right,0.05)

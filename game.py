3f7b9addeed16634348f0a04c9ffc77d63262e38
glennprays <glennsagala@gmail.com>
2023-08-27 05:02:59 +0700

    CODE REFACTORING

--- game.py
+++ game.py
@@ -6,114 +6,118 @@ from pgzero import clock
 import math
 import pygame.freetype
 
-TILE_SIZE = 32 
-WIDTH = 20 * TILE_SIZE 
-HEIGHT = 20 * TILE_SIZE 
+TILE_SIZE = 32
+ROWS = 20
+COLS = 20
 
-START_POS_X = 1 * TILE_SIZE
-START_POS_Y = 19 * TILE_SIZE
+WIDTH = ROWS * TILE_SIZE
+HEIGHT = COLS * TILE_SIZE
 
-END_POS_X = 19 * TILE_SIZE
-END_POS_Y = 1 * TILE_SIZE
+MID_POS = TILE_SIZE/2
 
-MID_POS = 16.5
-mimi = Actor('mimi')
-mimi.pos = START_POS_X+MID_POS,START_POS_Y+MID_POS
-tata = Actor('tata')
-tata.pos = END_POS_X+MID_POS,END_POS_Y+MID_POS
 
+# 0 = way
+# 1 = wall
+# mimi = main character
+# tata = target character
 maze = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, "tata"],
     [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
     [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
-    [1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1],
+    [1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1],
+    [1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1],
     [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
     [1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1],
-    [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+    [1, "enemy1", 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1],
     [1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-] 
+    [1, "mimi", 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
 
-win = False
+mimi = Actor("mimi")
+tata = Actor("tata")
+enemiy1 = Actor("jostrip")
+enemiy2 = Actor("jimot")
+
+# default Actor position
+for row in range(len(maze)):
+    for col in range(len(maze[row])):
+        current = maze[row][col]
+        pos = (col * TILE_SIZE)+MID_POS, (row * TILE_SIZE)+MID_POS
+        if current == "mimi":
+            mimi.pos = pos
+        if current == "tata":
+            tata.pos = pos
+
 
 def draw():
+    # winning checking
+    if mimi.distance_to(tata) == 0:
+        game_win()
+    else:
+        screen.clear()
+        screen.fill((70, 30, 50))
+        draw_map()
+        tata.draw()
+        mimi.draw()
+
+
+def draw_map():
+    for row in range(len(maze)):
+        for col in range(len(maze[row])):
+            current = maze[row][col]
+            current_x = TILE_SIZE * col
+            current_y = TILE_SIZE * row
+            if current == 1:
+                screen.blit("dirt_block", (current_x, current_y))
+
+def on_key_down(key):
+    # checking curren position
+    row = math.floor((mimi.y / TILE_SIZE))
+    column = math.floor((mimi.x / TILE_SIZE))
+    if key == keys.LEFT and column > 0:
+        column -= 1
+    elif key == keys.RIGHT and column < COLS-1:
+        column += 1
+    elif key == keys.UP and row > 0:
+        row -= 1
+    elif key == keys.DOWN and row < ROWS-1:
+        row += 1
+    
+    move_actor(mimi, row, column)
+
+def move_actor(actor, row, column):
+    # row and col start from 0 to n-1
+    # calculate target x and y position
+    X_POS = (column * TILE_SIZE) + MID_POS
+    Y_POS = (row * TILE_SIZE) + MID_POS
+
+    # prevent out of screen and wall checking
+    if X_POS >= 0 and X_POS <= WIDTH and Y_POS > 0 and Y_POS <= HEIGHT and maze[row][column] == 0:
+        actor.pos = X_POS, Y_POS
+
+def game_win():
     text_x = WIDTH / 3 + 10
     text_y = HEIGHT / 3
     text_z = WIDTH / 2 - 175
     text_k = HEIGHT / 2
 
-    if win:
-        screen.fill((0, 0, 0))  # Latar belakang hitam saat pemain menang
-        screen.draw.text("You Win!", (text_x, text_y), color=(255, 255, 255), fontsize=75)
-        screen.draw.text("Thank you for helping Mimi found tata", (text_z, text_k), color=(255, 255, 255), fontsize=30)
-
-    else:
-        screen.clear()
-        screen.fill((70, 30, 50))
-        create_map()
-        tata.draw()
-        mimi.draw()
-
-
-def create_map():
-    for row in range(len(maze)):
-        for col in range(len(maze[row])):
-            current = maze[row][col]
-            current_x = (TILE_SIZE * col)
-            current_y = (TILE_SIZE * row)
-            if current == 1:
-                screen.blit('dirt_block', (current_x, current_y))
-
-def move_up():
-    target = mimi.y - TILE_SIZE
-    if target > 0:
-        mimi.y = target
-def move_down():
-    target = mimi.y + TILE_SIZE
-    if target < HEIGHT:
-        mimi.y = target
-def move_left():
-    target = mimi.x - TILE_SIZE
-    if target > 0:
-        mimi.x = target
-def move_right():
-    target = mimi.x + TILE_SIZE
-    if target < WIDTH:
-        mimi.x = target
-
-def update():
-    global win 
-
-    coloumn = math.floor((mimi.x / TILE_SIZE))
-    row = math.floor((mimi.y / TILE_SIZE))
-    # TODO: FINISH WIN GAME
-    if coloumn == 19 and row == 1:
-        win = True 
-    
-    if win:
-        return
-        
-
-    elif keyboard.up and row-1 >= 0:
-        if maze[row-1][coloumn] == 0:
-            clock.schedule_unique(move_up,0.05)
-    elif keyboard.down and row+1 < (HEIGHT/TILE_SIZE):
-        if maze[row+1][coloumn] == 0:
-            clock.schedule_unique(move_down,0.05)
-    elif keyboard.left and coloumn-1 > 0:
-        if maze[row][coloumn-1] == 0:
-            clock.schedule_unique(move_left,0.05)
-    elif keyboard.right and coloumn+1 < (WIDTH/TILE_SIZE):
-        if maze[row][coloumn+1] == 0:
-            clock.schedule_unique(move_right,0.05)
+    screen.fill((0, 0, 0))  # make black backgroud
+    screen.draw.text(
+        "You Win!", (text_x, text_y), color=(255, 255, 255), fontsize=75
+    )
+    screen.draw.text(
+        "Thank you for helping Mimi find tata",
+        (text_z, text_k),
+        color=(255, 255, 255),
+        fontsize=30,
+    )

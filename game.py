2992f56609fc9bdde889707c2b65d57fe4b129e8
glennprays <glennsagala@gmail.com>
2023-08-27 11:42:40 +0700

    REFACTOR CODE

--- game.py
+++ game.py
@@ -13,14 +13,50 @@ COLS = 20
 WIDTH = ROWS * TILE_SIZE
 HEIGHT = COLS * TILE_SIZE
 
-MID_POS = TILE_SIZE/2
+MID_POS = TILE_SIZE / 2
 
-# 0 (way)
-# 1 (wall)
-# mimi (main character)
-# tata (target character)
-# enemy1 (enemy that move horizontal)
-# enemy2 (enemy that move vertical)
+# win or lose check
+lose = False
+win = False
+
+
+def calculate_coordinate(row, column):
+    x = column * TILE_SIZE
+    y = row * TILE_SIZE
+    return x, y
+
+
+def set_game():
+    global lose, win
+    win = False
+    lose = False
+    # set default Actor position
+    enemy_count = 1
+    for row in range(len(maze)):
+        for col in range(len(maze[row])):
+            current = maze[row][col]
+            pos = (col * TILE_SIZE) + MID_POS, (row * TILE_SIZE) + MID_POS
+            if current == "mimi":
+                mimi.pos = pos
+            elif current == "tata":
+                tata.pos = pos
+            elif current == "enemy":
+                name = "enemy_" + str(enemy_count)
+                enemy = Actor(name)
+                pos = calculate_coordinate(row, col)
+                enemy.x = pos[0] + MID_POS
+                enemy.y = pos[1] + MID_POS
+                enemies[name]["actor"] = enemy
+                enemy_count += 1
+
+
+"""
+0 (way)
+1 (wall)
+mimi (main character)
+tata (target character)
+enemy (enemy that move horizontal and vertical)
+"""
 maze = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "tata"],
@@ -45,59 +81,49 @@ maze = [
 ]
 
 enemies = {
-    "enemy_1": {
-        "direction": ["left"],
-        "velocity": 1.2
+    "enemy_1": {"direction": ["left"], "velocity": 1.2},
+    "enemy_2": {"direction": ["right"], "velocity": 0.9},
+    "enemy_3": {"direction": ["right"], "velocity": 1.8},
+    "enemy_4": {"direction": ["left"], "velocity": 2.3},
+    "enemy_5": {"direction": ["up"], "velocity": 1.6},
+}
+
+
+# text buttons with rectangle
+buttons = {
+    "restart": {
+        "width": 150,
+        "height": 50,
+        "x": (WIDTH - 150) / 2 - 80,
+        "y": (HEIGHT - 50) / 2 + 80,
+        "text": "Restart",
+        "font_size": 30,
+        "text_margin_top": 15,
+        "text_margin_left": 40,
+        "color": (255, 255, 255),
+        "bg_color": (0, 128, 0),
+        "callback": set_game,
     },
-    "enemy_2": {
-        "direction": ["right"],
-        "velocity": 0.9
-    },
-    "enemy_3": {
-        "direction": ["right"],
-        "velocity": 1.8
-    },
-    "enemy_4": {
-        "direction": ["left"],
-        "velocity": 2.3
-    },
-    "enemy_5": {
-        "direction": ["up"],
-        "velocity": 1.6
+    "exit": {
+        "width": 150,
+        "height": 50,
+        "x": (WIDTH - 150) / 2 + 100,
+        "y": (HEIGHT - 50) / 2 + 80,
+        "text": "Exit",
+        "font_size": 30,
+        "text_margin_top": 15,
+        "text_margin_left": 52,
+        "color": (255, 255, 255),
+        "bg_color": (74, 177, 255),
+        "callback": exit,
     },
 }
-enemies_direction = []
+
 
 mimi = Actor("mimi")
 tata = Actor("tata")
+set_game()
 
-def calculate_coordinate(row, column):
-    x = column * TILE_SIZE
-    y = row * TILE_SIZE
-    return x,y
-
-enemy_count = 1
-# default Actor position
-for row in range(len(maze)):
-    for col in range(len(maze[row])):
-        current = maze[row][col]
-        pos = (col * TILE_SIZE)+MID_POS, (row * TILE_SIZE)+MID_POS
-        if current == "mimi":
-            mimi.pos = pos
-        elif current == "tata":
-            tata.pos = pos
-        elif current == "enemy":
-            name = "enemy_"+str(enemy_count)
-            enemy = Actor(name)
-            pos = calculate_coordinate(row, col)
-            enemy.x = pos[0] + MID_POS
-            enemy.y = pos[1] + MID_POS
-            enemies[name]["actor"] = enemy
-            enemy_count += 1
-
-# win or lose check
-lose = False
-win = False
 
 def draw():
     global win, lose
@@ -107,14 +133,16 @@ def draw():
     elif win:
         game_win()
         return
+
     screen.clear()
     screen.fill((70, 30, 50))
     draw_map()
     tata.draw()
     mimi.draw()
-    
+
     for key, enemy in enemies.items():
-        enemy['actor'].draw()
+        enemy["actor"].draw()
+
 
 def draw_map():
     for row in range(len(maze)):
@@ -125,30 +153,32 @@ def draw_map():
             if current == 1:
                 screen.blit("dirt_block", (current_x, current_y))
 
+
 def on_key_down(key):
     global win, lose
     if win or lose:
         return
-    
+
     # checking current position
     row = math.floor((mimi.y / TILE_SIZE))
     column = math.floor((mimi.x / TILE_SIZE))
     if key == keys.LEFT and column > 0:
         column -= 1
-    elif key == keys.RIGHT and column < COLS-1:
+    elif key == keys.RIGHT and column < COLS - 1:
         column += 1
     elif key == keys.UP and row > 0:
         row -= 1
-    elif key == keys.DOWN and row < ROWS-1:
+    elif key == keys.DOWN and row < ROWS - 1:
         row += 1
-    
+
     move_actor(mimi, row, column)
+
 
 def update():
     global win, lose
     if win or lose:
         return
-    
+
     # enemy collition check
     for key, enemy in enemies.items():
         if enemy["actor"].distance_to(mimi) < TILE_SIZE:
@@ -164,6 +194,7 @@ def update():
     for key, enemy in enemies.items():
         enemy_move(enemy["actor"], enemy["direction"], enemy["velocity"])
 
+
 def move_actor(actor, row, column):
     # row and col start from 0 to n-1
 
@@ -172,8 +203,31 @@ def move_actor(actor, row, column):
     Y_POS = (row * TILE_SIZE) + MID_POS
 
     # prevent out of screen and wall checking
-    if X_POS >= 0 and X_POS <= WIDTH and Y_POS > 0 and Y_POS <= HEIGHT and maze[row][column] != 1:
+    if (
+        X_POS >= 0
+        and X_POS <= WIDTH
+        and Y_POS > 0
+        and Y_POS <= HEIGHT
+        and maze[row][column] != 1
+    ):
         actor.pos = X_POS, Y_POS
+
+
+def draw_button(button):
+    screen.draw.filled_rect(
+        Rect(button["x"], button["y"], button["width"], button["height"]),
+        button["bg_color"],
+    )
+    screen.draw.text(
+        button["text"],
+        (
+            button["x"] + button["text_margin_left"],
+            button["y"] + button["text_margin_top"],
+        ),
+        color=button["color"],
+        fontsize=button["font_size"],
+    )
+
 
 def game_win():
     text_x = (WIDTH / 3) + 10
@@ -182,157 +236,80 @@ def game_win():
     text_k = HEIGHT / 2
 
     screen.fill((0, 0, 0))  # make black backgroud
-    screen.draw.text(
-        "You Win!", (text_x, text_y), color=(255, 255, 255), fontsize=75
-    )
+    screen.draw.text("You Win!", (text_x, text_y), color=(255, 255, 255), fontsize=75)
     screen.draw.text(
         "Thank you for helping Mimi find Tata",
         (text_z, text_k),
         color=(255, 255, 255),
         fontsize=30,
     )
-    button_width = 150
-    button_height = 50
-    button_x = (WIDTH - button_width) / 2 - 80
-    button_y = (HEIGHT - button_height) / 2 + 80
+    draw_button(buttons["restart"])
+    draw_button(buttons["exit"])
 
-    screen.draw.filled_rect(Rect(button_x, button_y, button_width, button_height), (0, 128, 0))
-    screen.draw.text("Restart", (button_x + 40, button_y + 15), color=(255, 255, 255), fontsize=30)
-
-    button_width = 150
-    button_height = 50
-    button_z = (WIDTH - button_width) / 2 + 100
-    button_k = (HEIGHT - button_height) / 2 + 80
-
-    screen.draw.filled_rect(Rect(button_z, button_k, button_width, button_height), (74, 177, 255))
-    screen.draw.text("Exit", (button_z + 52, button_k + 15), color=(255, 255, 255), fontsize=30)
-    
 
 def gameover():
-    text_x = (WIDTH / 3)
+    text_x = WIDTH / 3
     text_y = HEIGHT / 3
-    text_z = (WIDTH / 2)- 85
+    text_z = (WIDTH / 2) - 85
     text_k = HEIGHT / 2
 
     screen.fill((0, 0, 0))  # make black backgroud
-    screen.draw.text(
-        "You Lose!", (text_x, text_y), color=(255, 255, 255), fontsize=75
-    )
+    screen.draw.text("You Lose!", (text_x, text_y), color=(255, 255, 255), fontsize=75)
     screen.draw.text(
         "Got caught by enemy",
         (text_z, text_k),
         color=(255, 255, 255),
         fontsize=30,
     )
-
-    button_width = 150
-    button_height = 50
-    button_x = (WIDTH - button_width) / 2 - 80
-    button_y = (HEIGHT - button_height) / 2 + 80
-
-    screen.draw.filled_rect(Rect(button_x, button_y, button_width, button_height), (0, 128, 0))
-    screen.draw.text("Restart", (button_x + 40, button_y + 15), color=(255, 255, 255), fontsize=30)
-
-    button_width = 150
-    button_height = 50
-    button_z = (WIDTH - button_width) / 2 + 100
-    button_k = (HEIGHT - button_height) / 2 + 80
-
-    screen.draw.filled_rect(Rect(button_z, button_k, button_width, button_height), (74, 177, 255))
-    screen.draw.text("Exit", (button_z + 52, button_k + 15), color=(255, 255, 255), fontsize=30)
+    draw_button(buttons["restart"])
+    draw_button(buttons["exit"])
 
 
 def enemy_move(enemy, direction, velocity):
     row = math.floor((enemy.y / TILE_SIZE))
     column = math.floor((enemy.x / TILE_SIZE))
 
+    threshold_x = calculate_coordinate(row, column)[0] + MID_POS
+    threshold_y = calculate_coordinate(row, column)[1] + MID_POS
+
     if direction[0] == "up":
-        next = maze[row-1][column]
-        threshold = calculate_coordinate(row, column)[1] + MID_POS
+        next = maze[row - 1][column]
         enemy.y -= velocity
-        if next == 1 and enemy.y <= threshold:
+        if next == 1 and enemy.y <= threshold_y:
             direction[0] = "down"
     elif direction[0] == "down":
-        next = maze[row+1][column]
+        next = maze[row + 1][column]
         threshold = calculate_coordinate(row, column)[1] + MID_POS
         enemy.y += velocity
-        if next == 1 and enemy.y >= threshold:
+        if next == 1 and enemy.y >= threshold_y:
             direction[0] = "up"
     elif direction[0] == "right":
-        next = maze[row][column+1]
-        threshold = calculate_coordinate(row, column)[0] + MID_POS
+        next = maze[row][column + 1]
         enemy.x += velocity
-        if next == 1 and enemy.x >= threshold:
+        if next == 1 and enemy.x >= threshold_x:
             direction[0] = "left"
     elif direction[0] == "left":
-        next = maze[row][column-1]
-        threshold = calculate_coordinate(row, column)[0] + MID_POS
+        next = maze[row][column - 1]
         enemy.x -= velocity
-        if next == 1 and enemy.x <= threshold:
+        if next == 1 and enemy.x <= threshold_x:
             direction[0] = "right"
 
-def restart_game():
-    global win, lose
-    win = False
-    lose = False
-
-    # Reset posisi Mimi dan Tata
-    for row in range(len(maze)):
-        for col in range(len(maze[row])):
-            current = maze[row][col]
-            pos = (col * TILE_SIZE) + MID_POS, (row * TILE_SIZE) + MID_POS
-            if current == "mimi":
-                mimi.pos = pos
-            elif current == "tata":
-                tata.pos = pos
-
-    # Reset posisi musuh
-    enemy_count = 1
-    for row in range(len(maze)):
-        for col in range(len(maze[row])):
-            current = maze[row][col]
-            if current == "enemy":
-                name = "enemy_" + str(enemy_count)
-                enemy = enemies[name]["actor"]
-                pos = calculate_coordinate(row, col)
-                enemy.x = pos[0] + MID_POS
-                enemy.y = pos[1] + MID_POS
-                enemy_count += 1
 
 def on_mouse_down(pos, button):
     global lose, win
-    if win or lose :
-        if button == mouse.LEFT and check_restart_button_click(pos) == True:
-            restart_game()
-        elif button == mouse.LEFT and check_exit_button_click(pos) == True:
-            exit()
+    if win or lose:
+        if button == mouse.LEFT:
+            if check_button_clicked(buttons["restart"], pos):
+                print("reset game")
+                set_game()
+            elif check_button_clicked(buttons["exit"], pos):
+                print("exit game")
+                exit()
+        return
 
 
-def check_restart_button_click(pos):
-    button_width2 = 150
-    button_height2 = 50
-    button_z = (WIDTH - button_width2) / 2 - 80
-    button_k = (HEIGHT - button_height2) / 2 + 80
-    
-    if (
-        (button_z <= pos[0] <= button_z + button_width2 and button_k <= pos[1] <= button_k + button_height2)
-    ):
-        return True
-    else:
-        return False
-
-def check_exit_button_click(pos):
-    button_width = 150
-    button_height = 50
-    button_z = (WIDTH - button_width) / 2 + 100
-    button_k = (HEIGHT - button_height) / 2 + 80
-    
-    if (
-        button_z <= pos[0] <= button_z + button_width and
-        button_k <= pos[1] <= button_k + button_height
-    ):
-        return True
-    else:
-        return False
-
-
+def check_button_clicked(button, pos):
+    return (
+        button["x"] <= pos[0] <= button["x"] + button["width"]
+        and button["y"] <= pos[1] <= button["y"] + button["height"]
+    )
